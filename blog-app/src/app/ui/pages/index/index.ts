6e4d94851d5d02af38df1a13e26c42a9443e8e92
Kan Vladimir <777itools@gmail.com>
2026-04-11 01:06:03 +0700

    Верстка перенесена в Angular

---
+++ blog-app/src/app/ui/pages/index/index.ts
@@ -0,0 +1,49 @@
+import { ChangeDetectionStrategy, Component, signal, computed } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CardBlog } from '../../components/CardBlog/CardBlog';
+
+@Component({
+  selector: 'app-index',
+  imports: [RouterLink, CardBlog],
+  templateUrl: 'index.html',
+  styleUrl: 'index.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Index { 
+  blogs = signal([
+    {
+        id: 1,
+        date: new Date(),
+        title: 'test1',
+        text: 'test1'
+    },
+    {
+        id: 2,
+        date: new Date(),
+        title: 'test2',
+        text: 'test2'
+    },
+    {
+        id: 3,
+        date: new Date(),
+        title: 'test2',
+        text: 'test2'
+    },
+    {
+        id: 4,
+        date: new Date(),
+        title: 'test2',
+        text: 'test2'
+    },
+    {
+        id: 5,
+        date: new Date(),
+        title: 'test2',
+        text: 'test2'
+    }
+  ] as any[]);
+
+  blogsLimit = computed(() => {
+    return this.blogs().slice(0, 2);
+  });
+}
