9f04fd615deca0389a268b56847be463a6c9127a
Vladimir <777itools@gmail.com>
2026-05-09 04:24:57 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/VKanIt/eltex-angular/commit/2e02fc8a4f683fd4ef013d9e052f1c6379c8e582
    GitHub Actions build: https://github.com/VKanIt/eltex-angular/actions/runs/25591611353

---
+++ chunk-3JVDMZQY.js
@@ -0,0 +1,1 @@
+import{T as o}from"./chunk-TUFQNFJN.js";var r=new o("\u0421\u0435\u0440\u0432\u0438\u0441 \u0434\u043B\u044F \u0440\u0430\u0431\u043E\u0442\u044B \u0441 localStorage");export{r as a};
